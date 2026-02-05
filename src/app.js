8fe14accfc056175cd135838fd20e07387c1da3b
mahmoud naser <mnasserone@gmail.com>
2026-02-05 13:25:08 +0300

    Creates a new NodeJs pp

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// create function great take name that return heell and name
+function great(name) {
+    return `Hello ${name}!`
+}
+
+module.exports = great; // export function great
+
+if (require.main === module) {  // if run file directly  
+    console.log(great("world")); // Hello world
+}
---
+++ src/test.sh
@@ -0,0 +1,11 @@
+EXPECTED="Hello, Test!" # expected output
+
+OUTPUT=$(node -e "console.log(require('./src/app.js')('Test'))") # run app.js with Test as argument
+
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+    echo "Test passed" # print message if test passed
+    exit 0 # exit with zero status to indicate success
+else
+    echo "Test failed"
+    exit 1 # exit with non-zero status to indicate failure
+fi  # finish test
