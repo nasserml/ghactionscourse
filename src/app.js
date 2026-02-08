77662a6e709555deedd556cae58f8de11d804043
mahmoud naser <mnasserone@gmail.com>
2026-02-08 09:04:44 +0300

    comments the code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 // create function great take name that return heell and name
 function great(name) {
     return `Hello, ${name}!`
-}
-
+} 
 module.exports = great; // export function great
 
 // if (require.main === module) {  // if run file directly  
